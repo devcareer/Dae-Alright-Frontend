c8d89d48fd11869d70ebf53a3ea4d17a5fd740a2
Idris <idrisaboladeo@gmail.com>
2019-10-21 10:52:34 +0100

    adds project structure: - uses create-react-app - uses bootstrap as a css framework - uses sass preprocessor for styling - installed radium incase of any inline styling

---
+++ dae-alright/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Radium, {StyleRoot} from 'radium';
+
+class App extends Component {
+
+  render(){
+  return (
+    <div className="App">
+      <h1>dae-Alright</h1>
+    </div>
+  );
+}
+}
+
+export default App;
