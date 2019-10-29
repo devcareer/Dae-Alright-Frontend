0173d2169d2fc9b292435576111991147a2607d7
Lario <osunkunleolanrewaju@gmail.com>
2019-10-29 15:04:32 +0100

    completed the reusable footer part

--- dae-alright/src/App.js
+++ dae-alright/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import SignUpModal from "./components/Modal/SignUpModal";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Footer from './../components/Footer/index'
+
+
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-      <h1>dae-Alright</h1>
-      <SignUpModal />
-      
+      <Footer />
+    
     </div>
   );
 }
