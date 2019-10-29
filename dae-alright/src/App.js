035aa25a0db004396a35799905c952e40735f6b1
Lario <osunkunleolanrewaju@gmail.com>
2019-10-29 15:04:32 +0100

    completed the reusable footer part

--- dae-alright/src/App.js
+++ dae-alright/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Footer from './../components/Footer/Footer'
 
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      <h1>dae-Alright</h1>
+      <Footer />
+    
     </div>
   );
 }
