3599e06b9a4ab421d3cef0c3ec3acd9d49abf001
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-10-31 21:14:39 +0100

    I create the modal component folder

---
+++ dae-alright/src/components/modal/custom-modal/custom-modal.component.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
+import "./custom-modal.styles.scss";
+
+const CustomModal = props => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Sign-Up now
+      </Button>
+
+      <Modal show={show} onHide={handleClose} className="custom-modal">
+        {props.title && <Modal.Title>{props.title()}</Modal.Title>}
+        {props.body && <Modal.Body>{props.body()}</Modal.Body>}
+        {props.footer && <Modal.Footer>{props.footer()}</Modal.Footer>}
+      </Modal>
+    </>
+  );
+};
+
+export default CustomModal;
---
+++ dae-alright/src/components/modal/signup-modal/signup-modal.component.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import CustomModal from "../custom-modal/custom-modal.component";
+import "./signup-modal.styles.scss";
+
+const btnStyles = {
+  background: "#D6DB46",
+  borderRadius: 0,
+  color: "#FFF",
+  padding: "0 1.5em",
+  width: "93%",
+  height: "3em",
+  margin: "1em 0"
+};
+const inputStyles = {
+  borderRadius: 0,
+  height: "3em"
+};
+
+const titleJSX = () => (
+  <React.Fragment>
+    <h2>Sign Up</h2>
+    <h6>
+      Hi,Welcome fill in the input to <br /> proceed shopping
+    </h6>
+  </React.Fragment>
+);
+
+const bodyJSX = () => (
+  <React.Fragment>
+    <form className="body--form">
+      <div className="form-input">
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="form-control"
+          style={inputStyles}
+        />
+      </div>
+      <div className="form-input">
+        <input
+          type="email"
+          placeholder="Email"
+          className="form-control"
+          style={inputStyles}
+        />
+      </div>
+      <div className="form-input">
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-control"
+          style={inputStyles}
+        />
+      </div>
+      <div className="form-input">
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="form-control"
+          style={inputStyles}
+        />
+      </div>
+      <button
+        style={btnStyles}
+        className="btn 
+             bnt-lg form-control"
+      >
+        Sign Up
+      </button>
+      <p>
+        Already Sign Up? <span>Sign In</span>
+      </p>
+    </form>
+  </React.Fragment>
+);
+
+const SignUpModal = () => {
+  return <CustomModal title={titleJSX} body={bodyJSX} />;
+};
+
+export default SignUpModal;
