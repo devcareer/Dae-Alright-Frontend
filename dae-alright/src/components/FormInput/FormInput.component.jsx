e639d464a4a946d8fef7f7cff1f72e14058db082
Idris <idrisaboladeo@gmail.com>
2019-11-01 13:20:52 +0100

    adds reusuable inputs and buttons

--- dae-alright/src/components/FormInput/FormInput.component.jsx
+++ dae-alright/src/components/FormInput/FormInput.component.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './formInput.style.scss';
 
-const FormInput = () => (
-  <div>
-    
-  </div>
-)
+const FormInput = ({placeholder, type, height, width}) => {
+    return(
+    <input className='input px-4'
+    type={type} 
+    placeholder={placeholder} style={{
+      height: height,
+      width: width,
+
+    }}/>
+    // <FormInput type={'password'} placeholder={'password'} height={'48px'} width={'15rem'}/>
+    )
+    }
 
 export default FormInput;
