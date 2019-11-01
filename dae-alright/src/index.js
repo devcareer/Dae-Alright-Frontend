e12d7c4f16955533bd046ed6c98d0fdcf2083be4
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-11-01 05:40:53 +0100

    make changes based on feedback

--- dae-alright/src/index.js
+++ dae-alright/src/index.js
@@ -10,7 +10,6 @@ ReactDOM.render(
     </Provider>, document.getElementById("root"));
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -18,7 +17,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
