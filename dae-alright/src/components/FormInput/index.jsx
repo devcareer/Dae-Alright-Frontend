aae0f32ce7a3bebbdea9aa241f946689d2bf21c8
Idris <idrisaboladeo@gmail.com>
2019-11-02 11:21:22 +0100

    made changes based on review: - adds prop type validation to component

---
+++ dae-alright/src/components/FormInput/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './formInput.style.scss';
+
+const FormInput = ({placeholder, type,style, renderProps}) => {
+    return(
+    <input 
+      onChange={renderProps}
+      className='input px-4'
+      type={type} 
+      placeholder={placeholder} 
+      style={{
+        ...style,
+      }}/>
+    
+    )
+    }
+FormInput.propTypes = {
+   onChange: PropTypes.func,
+   placeholder: PropTypes.string.isRequired,
+   type: PropTypes.string.isRequired
+   
+}
+
+export default FormInput;
