c6d06084495c4062633407995fb02889ef57f15f
lawal mukhtar olasukomi <lmukhtar29@gmail.com>
2019-11-05 12:44:12 +0000

    Dae Alright Header-Wrapper Adjusted

--- dae-alright/src/components/Header/HeaderWraper/index.js
+++ dae-alright/src/components/Header/HeaderWraper/index.js
@@ -2,18 +2,10 @@ import React from "react"
 import "./HeaderWraper.scss"
 
 function HeaderWraper(props) {
-    const styles = {
-        backgroundImage:`url("https://res.cloudinary.com/dtbjhs8a6/image/upload/v1572514768/my%20project/lr6xb0ipqmwtgny4lxtu.png")`,
-        height: "40em",
-        width: "100%",
-        backgroundRepeat: "no-repeat", 
-        backgroundPosition: "right"
-        
-    }
     return (
-        <div style={styles}>
-           
-        </div>
+        <header className="header-wrapper">
+            
+        </header>
 
     )
 }
