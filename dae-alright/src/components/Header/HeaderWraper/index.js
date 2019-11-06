bb14c9df49c9ed89d6e5e3092503848c132418dc
lawal mukhtar olasukomi <lmukhtar29@gmail.com>
2019-11-06 12:08:43 +0000

    Header wrapper new changes

--- dae-alright/src/components/Header/HeaderWraper/index.js
+++ dae-alright/src/components/Header/HeaderWraper/index.js
@@ -1,13 +1,20 @@
 import React from "react"
 import "./HeaderWraper.scss"
+import propTypes from "prop-types"
+
+
 
 function HeaderWraper(props) {
     return (
         <header className="header-wrapper">
-            
+            {props.renderProp()}
         </header>
 
     )
+}
+
+HeaderWraper.propTypes = {
+    renderProp: propTypes.func
 }
 
 
