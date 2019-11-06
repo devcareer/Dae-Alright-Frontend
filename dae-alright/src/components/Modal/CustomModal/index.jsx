dc4785905b76d2a2829d741bb4215d42452705ce
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-11-06 15:34:13 +0100

    I clear unwanted codes

--- dae-alright/src/components/Modal/CustomModal/index.jsx
+++ dae-alright/src/components/Modal/CustomModal/index.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Modal from "react-bootstrap/Modal";
-import { Button } from "react-bootstrap";
 import "./index.scss";
 
 const CustomModal = props => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  
 
   return (
     <>
-      {/* <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
-      <Modal show={show} onHide={handleClose} className="custom-modal">
+     
+      <Modal show={props.show} className="custom-modal">
         {props.title && <Modal.Title>{props.title()}</Modal.Title>}
         {props.body && <Modal.Body>{props.body()}</Modal.Body>}
         {props.footer && <Modal.Footer>{props.footer()}</Modal.Footer>}
