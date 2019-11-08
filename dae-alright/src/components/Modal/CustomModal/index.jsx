15f117c597f3c76dde73b2cfd758546991dab27a
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-08 13:12:25 +0100

    Sign In Modal complete

--- dae-alright/src/components/Modal/CustomModal/index.jsx
+++ dae-alright/src/components/Modal/CustomModal/index.jsx
@@ -6,15 +6,9 @@ import Button from 'react-bootstrap/Button'
 
 const CustomModal = props => {
   
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
     <>
-        <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+        
       <Modal show={show} onHide={handleClose} className="custom-modal">
         {props.title && <Modal.Title>{props.title()}</Modal.Title>}
         {props.body && <Modal.Body>{props.body()}</Modal.Body>}
