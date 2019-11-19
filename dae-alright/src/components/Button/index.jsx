b368b2968efbec8775026949a222625f6f335f0d
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-19 15:13:35 +0100

    integrate SignIn Authentication

--- dae-alright/src/components/Button/index.jsx
+++ dae-alright/src/components/Button/index.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.style.scss';
 const Button = ({text, style, icons, className}) => (
-   <button
-       className={`${className}`}
-       style={{
-       ...style,
-       padding: '3px',
-   }}>
-       { icons }
-       { text }
-    </button>
+  <button
+      className={${className}}
+      style={{
+      ...style,
+      padding: '3px',
+  }}>
+      { icons }
+      { text }
+   </button>
 )
 Button.propType = {
-   className: PropTypes.string,
-   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  text: PropTypes.string.isRequired,
 };
-export default Button;
