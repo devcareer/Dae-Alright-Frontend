c42281ccdc1a2a2893dce0105b4f8388ac3462dd
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-20 13:34:34 +0100

    Implemented SignIn with Email Auth

--- dae-alright/src/components/Modal/CustomModal/index.jsx
+++ dae-alright/src/components/Modal/CustomModal/index.jsx
@@ -4,12 +4,27 @@ import Modal from "react-bootstrap/Modal";
 import "./index.scss";
 
 const CustomModal = props => {
+<<<<<<< HEAD
   
   return (
     <>
      
 
       <Modal show={props.show} onHide={props.onHide} className="custom-modal">
+=======
+  const [show, setShow] = useState(false);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+  return (
+    <>
+     <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+        <Modal.Header closeButton></Modal.Header>
+      <Modal show={show} className="custom-modal">
+>>>>>>> Implemented SignIn with Email Auth
         {props.title && <Modal.Title>{props.title()}</Modal.Title>}
         {props.body && <Modal.Body>{props.body()}</Modal.Body>}
         {props.footer && <Modal.Footer>{props.footer()}</Modal.Footer>}
---
+++ dae-alright/src/redux/reducers/userReducer.js
@@ -0,0 +1,37 @@
+import SAMPLE_ACTION from "../constants/actionTypes"
+
+
+const initialState = {
+    user:{
+    address: '',
+    createdAt: '',
+    email:'',
+    firstName:'',
+    lastName:'',
+    phone: '',
+    id:'',
+    updatedAt:''
+    },
+    token: "",
+
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+
+        case SAMPLE_ACTION:
+             return action.payload
+
+
+        case SIGNIN_ACTION:
+            return{
+                ...state,
+                user:action.user,
+                token: action.token
+            }
+        default:
+            return state     
+    }
+
+}
+
