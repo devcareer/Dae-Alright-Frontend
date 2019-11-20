5a27ad1ed0d44b3d05cdc0d9679b9e52f4eec5e9
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-20 13:34:34 +0100

    Implemented SignIn with Email Auth

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import {Switch, Route} from 'react-router-dom'
 
 import Homepage from './Pages/Homepage'
 import UserDashboard from './Pages/user-dashboard';
-
-
-
-
+import SignIn from './components/Modal/SignInModal'
 
 class App extends Component {
 
