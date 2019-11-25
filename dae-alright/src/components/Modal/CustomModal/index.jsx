c8447cb77f0010d60aa594803dc3db17c81670d8
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-11-25 06:00:02 +0100

    Sign Up Authentication done!

--- dae-alright/src/components/Modal/CustomModal/index.jsx
+++ dae-alright/src/components/Modal/CustomModal/index.jsx
@@ -4,21 +4,12 @@ import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Modal from "react-bootstrap/Modal";
 import "./index.scss";
-import Button from 'react-bootstrap/Button' 
-
-
-const CustomModal = props => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+ 
+const CustomModal = (props) => {
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-      <Modal show={show} onHide={handleClose} className="custom-modal">
+      
+      <Modal show={props.show} className="custom-modal">
         {props.title && <Modal.Title>{props.title()}</Modal.Title>}
         {props.body && <Modal.Body>{props.body()}</Modal.Body>}
         {props.footer && <Modal.Footer>{props.footer()}</Modal.Footer>}
