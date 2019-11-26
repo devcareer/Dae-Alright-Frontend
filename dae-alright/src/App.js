b2ff2e651003e6516142be98a71359a47c8ddcdc
lawal mukhtar olasukomi <lmukhtar29@gmail.com>
2019-11-26 13:34:44 +0100

    data science form

--- dae-alright/src/App.js
+++ dae-alright/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare, faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
-
+import Form from "../src/components/form"
+import "../src/App.css"
 class App extends Component {
 
   render(){
   return (
     <div className="App">
-      <h1>dae-Alright</h1>
-      
+    <Form />    
     </div>
   );
 }
