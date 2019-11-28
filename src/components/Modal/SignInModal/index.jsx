a19a27fd502da555a5e495bfcf940720d6f341bc
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-28 10:49:03 +0100

    SignIn with Email Auth

--- src/components/Modal/SignInModal/index.jsx
+++ src/components/Modal/SignInModal/index.jsx
@@ -8,7 +8,7 @@ import {
   faFacebookSquare,
   faGooglePlusG
 } from "@fortawesome/free-brands-svg-icons";
-import { signIn } from '../../../redux/actions/user.signin'
+import { signIn } from '../../../redux/actions/user'
 
 
 class SignInModal extends Component {
