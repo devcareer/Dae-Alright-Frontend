7b2edd2bfda2ba1111956e5595e02c73c828351a
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-28 10:49:03 +0100

    SignIn with Email Auth

---
+++ dae-alright/src/components/Modal/CustomModal/index.jsx
@@ -0,0 +1,28 @@
+
+
+import React from "react";
+import PropTypes from "prop-types";
+import Modal from "react-bootstrap/Modal";
+import "./index.scss";
+
+
+const CustomModal = props => {
+
+
+  return (
+    <>
+    
+      <Modal show={props.show} className="custom-modal">
+        {props.title && <Modal.Title>{props.title()}</Modal.Title>}
+        {props.body && <Modal.Body>{props.body()}</Modal.Body>}
+        {props.footer && <Modal.Footer>{props.footer()}</Modal.Footer>}
+      </Modal>
+    </>
+  );
+};
+CustomModal.propTypes = {
+ title: PropTypes.func.isRequired,
+ body: PropTypes.func.isRequired,
+ footer: PropTypes.func
+};
+export default CustomModal; 
--- src/components/Modal/SignInModal/index.jsx
+++ src/components/Modal/SignInModal/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
 import CustomModal from "../CustomModal";
 import "./index.scss";
 import Button from "../../Button";
@@ -13,64 +14,25 @@ import { signIn } from '../../../redux/actions/user.signin'
 class SignInModal extends Component {
   constructor(){
     super()
-
     this.state={
-      SignInEmail: '',
-      SignInPassword: ''
+      email: '',
+      password: ''
     }
   }
   
   onEmailChange = e =>{
-    this.setState({SignInEmail: e.target.value})
+    this.setState({email: e.target.value})
     
   }
   onPasswordChange = e =>{
-    this.setState({SignInPassword: e.target.value})
+    this.setState({password: e.target.value})
     
   }
   
-  onloginSubmit=async (e)=>{
+  onloginSubmit=(e)=>{
     e.preventDefault()
-   const {SignInEmail, SignInPassword} = this.state;
-    const showAlert=(message, className)=> {
-      
-      const alert = document.createElement('alert');
-      alert.className = `alert ${className}`;
-      alert.appendChild(document.createTextNode(message));
-      const container = document.querySelector('.form-container');
-      const form = document.querySelector('.form');
-      container.insertBefore(alert, form);
-    }
-    if(SignInEmail && SignInPassword){
-     let error = document.querySelector('.alert')
-     console.log('Error!', error)
-     if (error){
-        error.remove()
-     }
-      
-      await fetch('https://dae-alright-staging.herokuapp.com/auth/signin',{
-      method: 'post',
-      headers:{'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        email: this.state.SignInEmail,
-        password: this.state.SignInPassword
-      })
-    })
-    .then(response=> response.json())
-    .then(response=> {
-      if(response.status==='success'){
-        console.log('Welldone Genius you rememebered your login credentials!')
-
-      }
-      else
-      showAlert('Incorrect Email or Password', alert)
-      }
-    
-    )
-  }
-else{
-    showAlert('Please Fill in all fields', alert)
-}
+   this.props.signIn(this.state.password,this.state.email)
+   
   }
 
   
@@ -108,8 +70,9 @@ const titleJSX = () => (
 
 const bodyJSX = () => (
   <React.Fragment>
-     <div className="form-container">
-    <div className='form'>
+     <div className='form'>
+{this.props.signInError && <div className='alert alert-danger' >{this.props.signInError }</div>}
+{this.props.signInSuccess && <div className="alert alert-success">{this.props.signInSuccess}</div>}
       <div className="form-input">
         <input
           type="text"
@@ -153,7 +116,9 @@ const bodyJSX = () => (
       >
         Login
         
-      </button>
+      </button>)}
+      
+      
       <p className="not-signed-in">
         Not Signed In? <span className="sign-up">Sign Up</span>
       </p>
@@ -184,11 +149,12 @@ const bodyJSX = () => (
         />
       </div>
     </div>
-    </div>
+ 
   </React.Fragment>
 );
-return <CustomModal title={titleJSX} body={bodyJSX} />;
+return <CustomModal show={this.props.showSignIn} title={titleJSX} body={bodyJSX} />;
 }
+
 };
 const mapStateToProps =(state)=>{
  
@@ -202,4 +168,4 @@ const mapStateToProps =(state)=>{
 }
 
 
-export default SignInModal
+export default connect(mapStateToProps, {signIn})(SignInModal)
