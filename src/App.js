b2a7ffd950fa24811bfbf22f979d17c8c4124bec
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-12-27 12:52:16 +0100

    implemented app routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { connect } from "react-redux"
-import Navigation from './components/Navigation/index'
-import AuthNav from "./components/Navigation/AuthNav"
-import SearchBar from './components/SearchBar';
-
-
-import SignIn from './components/Modal/SignInModal'
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import Navigation from "./components/Navigation/index";
+import AuthNav from "./components/Navigation/AuthNav";
+import SearchBar from "./components/SearchBar";
+import SignIn from "./components/Modal/SignInModal";
 
 class App extends Component {
-
-  render(){
-  return (
-
-    <div className="App">
-     <Navigation/>
-     <AuthNav/>
-     <SignIn/>
-     
-    
-    </div>
-  
-  );
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <AuthNav />
+        <SignIn />
+        <Switch>
+        
+        </Switch>
+      </div>
+    );
+  }
 }
 
-}
-
-export default connect() (App);
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./store";
-
+import {BroswerRouter} from 'react-router-dom'
 ReactDOM.render(
+  
   <Provider store={configureStore()}>
+    <BroswerRouter>
     <App />
+    </BroswerRouter>
   </Provider>,
   document.getElementById("root")
 );
