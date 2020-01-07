bd8ac504b1c9cfaf1fb5e2b0fc33c7ec9ba172e8
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-01-07 22:39:01 +0100

    review order page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Switch, Route} from 'react-router-dom'
 import RestauMenuPage from './Pages/Restaurant/RestauMenuPage'
 import Homepage from './Pages/Homepage'
 import UserDashboard from './Pages/user-dashboard';
-
-
+import ReviewOrder from './Pages/ReviewOrder'
+import PaymentPage from './Pages/payment-page'
 
 
 
@@ -19,6 +19,8 @@ class App extends Component {
         <Route exact  path="/" component={Homepage} />
         <Route exact path="/dashboard/information" component={UserDashboard} />
         <Route exact path="/restaurant/menu" component={RestauMenuPage} />
+        <Route exact path="/revieworder" component={ReviewOrder} />
+        <Route exact path="/payment" component={PaymentPage} />
      </Switch> 
     
     </div>
