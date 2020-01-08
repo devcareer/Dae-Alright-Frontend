b5c378c6b5f02e383911a0c5cf9e16eeeb7a2ce2
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-01-08 15:10:44 +0100

    çheckout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import RestauMenuPage from './Pages/Restaurant/RestauMenuPage'
 import Homepage from './Pages/Homepage'
 import UserDashboard from './Pages/user-dashboard';
 import ReviewOrder from './Pages/ReviewOrder'
-import PaymentPage from './Pages/payment-page'
 
 
 
@@ -20,7 +19,6 @@ class App extends Component {
         <Route exact path="/dashboard/information" component={UserDashboard} />
         <Route exact path="/restaurant/menu" component={RestauMenuPage} />
         <Route exact path="/revieworder" component={ReviewOrder} />
-        <Route exact path="/payment" component={PaymentPage} />
      </Switch> 
     
     </div>
