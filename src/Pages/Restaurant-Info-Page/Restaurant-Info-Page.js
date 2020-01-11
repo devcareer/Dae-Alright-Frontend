996070ef3905655a06413f878c06d2ea675ebe53
Lario <osunkunleolanrewaju@gmail.com>
2020-01-11 19:24:17 +0100

    coverts the titles to tabs for easy navigation and created components for each of the contents of the tabs

--- src/Pages/Restaurant-Info-Page/Restaurant-Info-Page.js
+++ src/Pages/Restaurant-Info-Page/Restaurant-Info-Page.js
@@ -1,6 +1,14 @@
 import React from 'react'
+
+import {Tabs, Tab, TabList, TabPanel} from 'react-tabs'
+
+import 'react-tabs/style/react-tabs.css'
 import './Restaurant-Info-Page.style.scss'
+
 import Footer from '../../components/Footer/index'
+import RestaurantInfoTabThree from '../../components/Restaurant-info-tabs/restaurant-info-tab-three'
+import RestaurantInfoTabTwo from '../../components/Restaurant-info-tabs/restaurant-info-tab-two'
+import RestaurantInfoTabOne from '../../components/Restaurant-info-tabs/restaurant-info-tab-one'
 
 function RestaurantInfo() {
     return (
@@ -8,7 +16,7 @@ function RestaurantInfo() {
             <header className='restaurant-page-header'></header>
             <main className='restaurant-page-main'>
                 <div className='left-main-section'></div>
-                <div className='middle-main-section'>
+                {/* <div className='middle-main-section'>
                     <div className='middle-main-section-titles'>
                         <div className='main-section-titles menu'>
                             <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/ayirh2x9edkkqqhy7gjb.png'/> Menu </p>
@@ -20,54 +28,35 @@ function RestaurantInfo() {
                             <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501477/xuqj0atynsjwzla4ubhk.png'/> Restaurant Info </p>
                         </div>
                     </div>
-                    <div className='middle-main-section-bodies'>
-                        <div className='main-address-details'>
-                            <h4>Overview Ajegunle Finest Chao</h4>
-                            <p>
-                                Base prepared fresg daily. Extra toppings are availbale in chose extra.
-                                Choose ur sauce: Go for BBQ sauce or piri piri suace with your pizza base for no extra cost. Choose you cut: triangular, square, finges.
-                            </p>
-                        </div>
-                        <div className='main-information-details'>
-                            <div className='contact-details'>
-                                <h4>Contact Details</h4>
-                                <p>
-                                    <img  alt='location icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/qkfwznycw2rapvpzysur.png'/>
-                                    <span>Pizza Express Restaurants, Boundary, Ajegunle.</span>
-                                </p>
-                                <p>
-                                    <img  alt='phone icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/qkfwznycw2rapvpzysur.png'/>
-                                    <span>07037822540</span>
-                                </p>
-                                <p>
-                                    <img  alt='world wide web icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/qkfwznycw2rapvpzysur.png'/>
-                                    <span><a href='www.ajegunlefinestfood.co'>http://www.ajegunlefinestfood.co</a></span>
-                                </p>
-                                <p>
-                                    <img  alt='email icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/qkfwznycw2rapvpzysur.png'/>
-                                    <span><a href='www.gmail.com'>Send Enquiry by Email</a></span>
-                                </p>
-                            </div>
-                            <div className='opening-hours'>
-                                <h4>Opening Hours</h4>
-                                <div className="days-and-hour-listed">
-                                    <select name="browsers" size="1" class="days-and-hour">
-                                        <option>Monday  11am to 11pm</option>
-                                        <option value="chrome">Tuesday  11am to 11pm</option>
-                                        <option value="safari">Wednesday  11am to 11pm</option>
-                                        <option value="explorer">Thursday  11am to 11pm</option>
-                                        <option value="firefox">Friday  11am to 11pm</option>
-                                        <option value="opera">Saturday  11am to 11pm</option>
-                                        <option value="brave">Sunday  11am to 11pm </option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='main-scan-code'>
-                            <img alt='scan code' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501585/dylau8bpz2nqnqoizwj2.png' />
-                        </div>
-                    </div>
-                </div>
+                    
+                    <RestaurantInfoTabThree />
+
+                </div> */}
+                <Tabs className='middle-main-section'>
+                    <TabList className='middle-main-section-titles'>
+                        <Tab className='main-section-titles menu'>
+                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/ayirh2x9edkkqqhy7gjb.png'/> Menu </p>
+                        </Tab>
+                        <Tab className='main-section-titles reviews'>
+                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501477/qp96zwcecs2lfdu36ee8.png'/> Reviews (1) </p>
+                        </Tab>
+                        <Tab className='main-section-titles restaurant-info'>
+                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501477/xuqj0atynsjwzla4ubhk.png'/> Restaurant Info </p>
+                        </Tab>
+                    </TabList>
+                    <TabPanel>
+                        <RestaurantInfoTabOne />
+                    </TabPanel>
+                    <TabPanel>
+                        <RestaurantInfoTabTwo />
+                    </TabPanel>
+                    <TabPanel>
+                        <RestaurantInfoTabThree />
+                    </TabPanel>
+                    
+                    
+
+                </Tabs>
                 <div className='right-main-section'></div>
             </main>
             
---
+++ src/components/Restaurant-info-tabs/restaurant-info-tab-one.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './restaurant-info-tab-one.styles.scss'
+
+
+const RestaurantInfoTabOne = () => (
+    <div className='middle-main-section-bodies'>
+        <h2>One</h2>
+    </div>
+)
+
+export default RestaurantInfoTabOne 
