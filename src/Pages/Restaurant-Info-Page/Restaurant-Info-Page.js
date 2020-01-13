d483ae35901cbb1c3260080f8ca957aabaca3155
Lario <osunkunleolanrewaju@gmail.com>
2020-01-13 05:45:12 +0100

    menu tab content added

--- src/Pages/Restaurant-Info-Page/Restaurant-Info-Page.js
+++ src/Pages/Restaurant-Info-Page/Restaurant-Info-Page.js
@@ -15,23 +15,8 @@ function RestaurantInfo() {
         <body className='restaurant-page'>
             <header className='restaurant-page-header'></header>
             <main className='restaurant-page-main'>
-                <div className='left-main-section'></div>
-                {/* <div className='middle-main-section'>
-                    <div className='middle-main-section-titles'>
-                        <div className='main-section-titles menu'>
-                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577444808/ayirh2x9edkkqqhy7gjb.png'/> Menu </p>
-                        </div>
-                        <div className='main-section-titles reviews'>
-                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501477/qp96zwcecs2lfdu36ee8.png'/> Reviews (1) </p>
-                        </div>
-                        <div className='main-section-titles restaurant-info'>
-                            <p><img  alt='menu icon' src='https://res.cloudinary.com/dtbjhs8a6/image/upload/v1577501477/xuqj0atynsjwzla4ubhk.png'/> Restaurant Info </p>
-                        </div>
-                    </div>
-                    
-                    <RestaurantInfoTabThree />
+                <div className='left-main-section'> Left</div>
 
-                </div> */}
                 <Tabs className='middle-main-section'>
                     <TabList className='middle-main-section-titles'>
                         <Tab className='main-section-titles menu'>
@@ -55,7 +40,7 @@ function RestaurantInfo() {
                     </TabPanel>
 
                 </Tabs>
-                <div className='right-main-section'></div>
+                <div className='right-main-section'>right</div>
             </main>
             
             <Footer />
--- src/components/Restaurant-info-tabs/restaurant-info-tab-one.jsx
+++ src/components/Restaurant-info-tabs/restaurant-info-tab-one.jsx
@@ -2,10 +2,116 @@ import React from 'react'
 
 import './restaurant-info-tab-one.styles.scss'
 
+import Button from'../Button/index'
+
 
 const RestaurantInfoTabOne = () => (
     <div className='middle-main-section-bodies'>
-        <h2>One</h2>
+        <div className='tab-one-title'>
+            <p className='find-assorted-food'>Find Food</p>
+            <div><img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578886160/vzqt40cltledqojyqouj.png" alt="search-assorted-food" className='search-assorted-food' /></div>
+        </div>
+
+        <p className='menu-description'>please note that you can individually add item to cart of you can just add all item too cart below</p>
+
+        <h2 className='menu-title'>Local Assorted </h2>
+
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        <div className='local-assorted-card'>
+            <div className='local-assorted-description'>
+                <div className='for-local-assorted-picture'>
+                    <img src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1578128272/gffs5uctqukn7l6nj9wd.png" alt="local-assorted-picture" className='local-assorted-picture' />
+                </div>
+                <div className='for-local-assorted-description'>
+                    <h3 className='local-assorted-description-one'>Chicken Tandoree Special 12" Dee Pan</h3>
+                    <p className='local-assorted-description-two' >Cheese, Tomatoes and Italians herbs</p>
+                </div>
+            </div>
+            <div className='for-local-assorted-price'>
+                <h3 className='local-assorted-price' >#400</h3>
+                <Button className='local-assoted-button' text='Add to Cart' />
+            </div>
+        </div>
+        
+        <div className='for-local-assorted-main-button'>
+            <button className='local-assorted-main-button'>
+                Add To Cart
+            </button>
+        </div>
     </div>
 )
 
